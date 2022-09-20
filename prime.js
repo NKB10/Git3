ebedd89ca5a8b981f61a463e8763a12c8c0b312a
Nitish kumar <nitish514810@gmail.com>
2022-09-20 18:12:16 +0530

    edit code orime

--- prime.js
+++ prime.js
@@ -1,10 +1,10 @@
 function prime(num){
     if(num<0 ||num==undefined){
-        return true
+        return false
     }
     for(let i=2;i<=num**0.5;i++){
         if(num%i==0){
-            return true
+            return false
         }
     }
     return true
